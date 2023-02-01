52ec5d4e05679271b6f3632a90a41f5f0bebaf4a
LeonardoMendes007 <mendesleo98@gmail.com>
2023-01-31 23:49:18 -0300

    First Commit

---
+++ Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost", Port = 5672 };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "Leonardo",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: "Leonardo",
+                     basicProperties: null,
+                     body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ExemploRabbitMQ
 Exemplo de producer e consumer utilizando RabbitMQ
+
+## Para executar:
+Criar e rodar um container RabbitMQ
+
+docker run -d --name rabbitmq-exemplo -p 5672:5672 -p 5673:5673 -p 15672:15672 rabbitmq:3.8-management
+
+Restaurar os pacotes das aplicações
+
+dotnet restore .\Producer\
+dotnet restore .\Consumer\
+
+Executar o produtor e o Consumidor
+
+dotnet run .\Producer\
+dotnet run .\Consumer\
